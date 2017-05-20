5fdf6c9c73fc8f8295501f90d45cc1450cf397d0
Courtland Caldwell <courtland.caldwell@dominodatalab.com>
2017-05-19 22:05:36 -0700

    Add uncurry

--- fpinscala/src/test/scala/ch2/CurrySpec.scala
+++ fpinscala/src/test/scala/ch2/CurrySpec.scala
@@ -6,4 +6,9 @@ class CurrySpec extends FlatSpec with Matchers {
   "curry" should "work for addition" in {
     Curry.curry((x: Int, y: Int) => x + y)(4)(5) shouldEqual 9
   }
+
+  "uncurry" should "reverse a currying" in {
+    val curriedAddition = Curry.curry((x: Int, y: Int) => x + y)
+    Curry.uncurry(curriedAddition)(4, 5) shouldEqual 9
+  }
 }
